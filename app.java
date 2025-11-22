025e72247b7e343aade97e764edf63f5c91e0ce7
worldofkrrish24 <krishnaindigi24@gmail.com>
2025-11-22 16:56:45 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,10 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+// dev-2 pushed from local
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+    }
+}
