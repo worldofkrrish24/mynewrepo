ed5fe1916dd4ecd86e9ead859fb45d7572634235
root <root@ip-172-31-72-166.ec2.internal>
2025-11-22 11:46:18 +0000

    i am adding data

--- app.java
+++ app.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+<<<<<<< Upnated up
 }
 
 // dev-2 pushed from local
@@ -10,3 +11,6 @@ public class HelloWorld {
         System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
     }
 }
+=======
+new code by developer one without pull the code
+>>>>>>> Stashed changes
